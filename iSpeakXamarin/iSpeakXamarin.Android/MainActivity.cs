82204b8dad638133e58c8fec736bd1857bd458ae
RQ <ricky.dwsystems@gmail.com>
2022-05-27 12:41:08 +0700

    updated about page. changed app icon label to iSpeak. rollout to production

--- iSpeakXamarin/iSpeakXamarin.Android/MainActivity.cs
+++ iSpeakXamarin/iSpeakXamarin.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace iSpeakXamarin.Droid
 {
-    [Activity(Label = "iSpeakXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "iSpeak", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
